fb7f78907a831b6d89f5e02bdb498ec48895e185
Quinton Mills <41127568+quintonmills@users.noreply.github.com>
2022-05-20 22:15:34 -0400

    Add files via upload

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,17 +1,22 @@
 import { useState } from 'react';
 import { Alert } from 'react-native-web';
+import { useContext } from 'react/cjs/react.production.min';
 
 import AuthContent from '../components/Auth/AuthContent';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { AuthContext } from '../store/auth-context';
 import { login } from '../util/auth';
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
+  const authCtx = useContext(AuthContext);
+
   async function loginHandler({ email, password }) {
     setIsAuthenticating(true);
     try{
-      await login(email, password);
+      const token = await login(email, password);
+      authCtx.authenticate(token);
     }
     catch (error) {
       Alert.alert(
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,27 @@
+import axios from 'axios'
 import { StyleSheet, Text, View } from 'react-native';
+import { useContext, useEffect } from 'react/cjs/react.production.min';
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState('');
+
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+useEffect(() => {
+  axios.get('https://react-native-course-b8df4-default-rtdb.firebaseio.com/message.json?auth=' + 
+    token
+  ).then((response) =>{
+    setFetchedMEssage(response.data);
+  });
+}, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
